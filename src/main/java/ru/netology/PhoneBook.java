84b30093fe815cf8dd77919dc7ee44269eb89a63
Vadim <alekseevvs_sstu@mail.ru>
2025-06-13 10:04:16 +0400

    add implemented

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -17,6 +17,13 @@ public class PhoneBook {
     }
 
     public int add(String name, String number) {
-        return 0;
+        if (!contactsByName.containsKey(name)) {//проверяем наличие имени в книгеAdd commentMore actions
+            contactsByName.put(name, number);//если нет добавляем в мапу в качестве ключа выступает имя
+            contactsByNumber.put(number, name);//в эту мапу добавляем в качестве ключа выступает номер
+            allNames.add(name);//добавляем контакт в коллекцию
+            return contactsByName.size();//возвращается количество контактов
+        } else {
+            return -1; // если имя существует, то возвращается -1 контакт не добавлен
+        }
     }
 }
