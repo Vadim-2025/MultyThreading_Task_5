bf1d7a9e3f56bd9ba774e1d456ea298883946eb1
Vadim <alekseevvs_sstu@mail.ru>
2025-06-13 09:57:55 +0400

    Stub and tests for add

--- src/main/java/ru/netology/PhoneBook.java
+++ src/main/java/ru/netology/PhoneBook.java
@@ -1,4 +1,22 @@
 package ru.netology;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 public class PhoneBook {
+    public Map<String, String> contactsByName;
+    public Map<String, String> contactsByNumber;
+    public SortedSet<String> allNames;
+
+    public PhoneBook() {
+        contactsByName = new HashMap<>();
+        contactsByNumber = new HashMap<>();
+        allNames = new TreeSet<>();
+    }
+
+    public int add(String name, String number) {
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,2 +1,15 @@
+import org.junit.jupiter.api.Test;
+import ru.netology.PhoneBook;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PhoneBookTest {
+    @Test
+    public void testAdd() {
+        PhoneBook phoneBook = new PhoneBook();
+        assertEquals(1, phoneBook.add("Алиса", "899989"));
+        assertEquals(-1, phoneBook.add("Алиса", "766554")); // Повторяющееся имя
+        assertEquals(2, phoneBook.add("Роберт", "45676754"));
+        assertEquals(3, phoneBook.add("Карл", "6654457"));
+    }
 }
